5d01717b7aa8905557d10d4dc2939a210669f463
Chris Peakin <christopher.peakin@googlemail.com>
2022-04-30 10:43:51 +0100

    cleaned up Home test

--- my-app/src/Tests/Components/Home.test.js
+++ my-app/src/Tests/Components/Home.test.js
@@ -1,8 +1,8 @@
-import Home from '../../Components/Home';
 import React from "react";
 import renderer from "react-test-renderer";
+import Home from '../../Components/Home';
 
 test("renders the Home component correctly", () => {
-    const testRenderer = renderer.create(<Home />);
-    expect(testRenderer).toMatchSnapshot();
+  const testRenderer = renderer.create(<Home />);
+  expect(testRenderer).toMatchSnapshot();
 });
