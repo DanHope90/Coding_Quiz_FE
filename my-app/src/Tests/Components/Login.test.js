cf3cb3f7c14e7d760c48c44076f05241e5c9f5f5
Chris Peakin <christopher.peakin@googlemail.com>
2022-05-02 12:18:13 +0100

    corrected typos in new tests

--- my-app/src/Tests/Components/Login.test.js
+++ my-app/src/Tests/Components/Login.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import renderer from "react-test-renderer";
 import Login from "../../Components/Login";
 
-test("renders the Home component correctly", () => {
+test("renders the Login component correctly", () => {
   const testRenderer = renderer.create(<Login />);
   expect(testRenderer).toMatchSnapshot();
 });
--- my-app/src/Tests/Components/Register.test.js
+++ my-app/src/Tests/Components/Register.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import renderer from "react-test-renderer";
 import Register from "../../Components/Register";
 
-test("renders the Home component correctly", () => {
+test("renders the Register component correctly", () => {
   const testRenderer = renderer.create(<Register />);
   expect(testRenderer).toMatchSnapshot();
 });
