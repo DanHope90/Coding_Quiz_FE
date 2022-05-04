df66e84e57e5432d9768df0eca9a04e7ce2f71fb
Chris Peakin <christopher.peakin@googlemail.com>
2022-05-04 16:04:17 +0100

    3 quizzes in database, now 3 quiz buttons rendering

--- my-app/src/Components/TakeAQuiz.js
+++ my-app/src/Components/TakeAQuiz.js
@@ -1,16 +1,52 @@
-import React from "react";
+/* eslint-disable no-console */
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import PropTypes from "prop-types";
 import TakeAQuizCard from "./TakeAQuizCard";
-import data from "../Data/TakeQuizzes.json";
 import "../Styles/TakeAQuiz.css";
 
-function TakeAQuiz() {
-  const { quizzes } = data;
+function TakeAQuiz(quizId) {
+  // Below sets the initial alert.
+  const initialState = {
+    alert: { message: "", isSuccess: false },
+  };
+
+  const [alert, setAlert] = useState(initialState.alert);
+
+  // Below employs an empty array to store all quizzes, useEffect makes an axios request
+  // to populate said array with all quizzes. This may not be needed later.
+  const [availableQuizzes, setAvailableQuizzes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/api/quizzes")
+      .then(({ data }) => setAvailableQuizzes(data))
+      .catch(() => setAlert({ message: "Axios promise rejected", isSuccess: false }));
+  }, []);
+
+  console.log(availableQuizzes);
+  console.log(initialState);
+
+  // Below employs an empty object to store our desired quiz.
+  // useEffect makes an axios request to populate object with
+  // the quiz we have chosen, using quizId as a prop passed down.
+
+  const [desiredQuiz, setDesiredQuiz] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/api/quizzes/${quizId}`)
+      .then(({ data }) => setDesiredQuiz(data))
+      .catch(() => setAlert({ message: "Axios promise rejected", isSuccess: false }));
+  });
+
+  console.log(desiredQuiz);
+
   return (
     <>
       <div>This is the Take A Quiz Page</div>
       <div className="quiz-array">
-        {quizzes.map((quiz) => (
+        {availableQuizzes.map((quiz) => (
           <div key={quiz.quizName} className="indiv.quiz">
             <TakeAQuizCard quizName={quiz.quizName} quizURL={quiz.quizURL} />
           </div>
--- my-app/src/Components/TestQuiz.js
+++ my-app/src/Components/TestQuiz.js
@@ -1,31 +1,11 @@
-/* eslint-disable no-console */
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
 function TestQuiz() {
-  // Below sets the initial alert.
-  const initialState = {
-    alert: { message: "", isSuccess: false },
-  };
-
-  const [alert, setAlert] = useState(initialState.alert);
-
-  // Below employs an empty array to store all quizzes, useEffect makes an axios request
-  // to populate said array with all quizzes.
-  const [availableQuizzes, setAvailableQuizzes] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:4000/api/quizzes")
-      .then(({ data }) => setAvailableQuizzes(data))
-      .catch(() => setAlert({ message: "Axios promise rejected", isSuccess: false }));
-  }, []);
-
-  console.log(availableQuizzes);
-  console.log(initialState);
-
   return (
-    <div>This is a test quiz!</div>
+    <>
+      <div>{alert.message}</div>
+      <div>This is a test quiz!</div>
+    </>
   );
 }
 
