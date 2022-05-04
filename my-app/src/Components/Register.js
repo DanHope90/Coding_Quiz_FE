62727ec6e2006441ef2f05adc7311bff3492174f
Chris Peakin <christopher.peakin@googlemail.com>
2022-05-04 10:51:09 +0100

    updated styling on register

--- my-app/src/Components/Register.js
+++ my-app/src/Components/Register.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "../Styles/Register.css";
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -25,12 +26,12 @@ function Register() {
 
   return (
     <>
-      <div>
+      <div className="titles">
         <h1>Register</h1>
         <h2>Please create an account</h2>
       </div>
       <div>
-        <form onSubmit={onSubmit}>
+        <form className="form" onSubmit={onSubmit}>
           <input type="text" className="form-control" id="userName" name="userName" value={userName} placeholder="Enter username" onChange={onChange} />
           <input type="email" className="form-control" id="email" name="email" value={email} placeholder="Enter email address" onChange={onChange} />
           <input type="password" className="form-control" id="password" name="password" value={password} placeholder="Enter password" onChange={onChange} />
