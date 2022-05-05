ac51fab2c8ad5edca32e05c6574f8ba87e714468
Chris Peakin <christopher.peakin@googlemail.com>
2022-05-05 19:27:44 +0100

    updated tests

--- my-app/src/Tests/Components/TakeAQuiz.test.js
+++ my-app/src/Tests/Components/TakeAQuiz.test.js
@@ -1,8 +1,24 @@
-import React from "react";
+/* eslint-disable react/jsx-no-constructed-context-values */
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import renderer from "react-test-renderer";
 import TakeAQuiz from "../../Components/TakeAQuiz";
+import Context from "../../Utils/Context";
 
 test("renders the TakeAQuiz component correctly", () => {
-  const testRenderer = renderer.create(<TakeAQuiz />);
+  // Below employs an empty array to store all quizzes, useEffect makes an axios request
+  // to populate said array with all quizzes.
+  const [availableQuizzes, setAvailableQuizzes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/api/quizzes")
+      .then(({ data }) => setAvailableQuizzes(data));
+  }, []);
+  const testRenderer = renderer.create(
+    <Context.Provider value={{ availableQuizzes }}>
+      <TakeAQuiz />
+    </Context.Provider>,
+  );
   expect(testRenderer).toMatchSnapshot();
 });
